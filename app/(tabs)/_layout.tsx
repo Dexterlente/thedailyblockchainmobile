825af4ca9b2454d7a4802bd358ad7927ab3f9681
dexter <dexter.lente18@gmail.com>
2024-12-11 00:50:52 +0800

    initialize

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -30,14 +30,21 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={20} name="house.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="liveprices"
+        options={{
+          title: 'Liveprices',
+          tabBarIcon: ({ color }) => <IconSymbol size={20} name="paperplane.fill" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={20} name="paperplane.fill" color={color} />,
         }}
       />
     </Tabs>
